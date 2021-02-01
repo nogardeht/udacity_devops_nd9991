dc6c6765ed888d301f7db7f655de2d0e937e3104
Diego Marciano <thedragonsreborn@hotmail.com>
2021-01-31 22:34:50 -0300

    Unit 3 Lesson 3 completed, upload-stack.sh updated to include force option that deletes and recreates the stack

--- Unit 3 - Infrastructure as Code/Lesson 3/upload-stack.sh
+++ Unit 3 - Infrastructure as Code/Lesson 3/upload-stack.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-if [[ $# -eq 4 ]]
+if [[ $# -ge 4 ]]
 then
-    existingStack=$(aws cloudformation describe-stacks --stack-name ${1} --output text --query "Stacks[*].{Name:StackName}")
+    stackStatus=$(aws cloudformation describe-stacks --stack-name ${1} --output text --query "Stacks[*].{Status:StackStatus}")
     if [[ $? -eq 0 ]]
     then
         aws cloudformation update-stack --stack-name ${1} --template-body file://${2} --parameters file://${3} --region=${4}
-    else
-        aws cloudformation create-stack --stack-name ${1} --template-body file://${2} --parameters file://${3} --region=${4}
+        if [[ $? -eq 0 ]]
+        then
+            exit 0
+        elif [[ ${5} == "-f" ]]
+        then
+            aws cloudformation delete-stack --stack-name ${1} --region=${4}
+            sleep 10
+            stackStatus=$(aws cloudformation describe-stacks --stack-name ${1} --output text --query "Stacks[*].{Status:StackStatus}")
+            while [[ $stackStatus == "DELETE_IN_PROGRESS" ]]
+            do
+                sleep 5
+                stackStatus=$(aws cloudformation describe-stacks --stack-name ${1} --output text --query "Stacks[*].{Status:StackStatus}")
+            done
+        fi
     fi
+    aws cloudformation create-stack --stack-name ${1} --template-body file://${2} --parameters file://${3} --region=${4}
 else
-    echo "usage: ./upload-stack.sh <stack_name> <filename> <parameters> <region>"
+    echo "usage: ./upload-stack.sh <stack_name> <filename> <parameters> <region> [-f]"
 fi
+exit $?
