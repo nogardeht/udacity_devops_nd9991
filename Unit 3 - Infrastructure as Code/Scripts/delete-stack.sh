cff650bcd7a9dba1246814efab2801159b45ee26
Diego Marciano <thedragonsreborn@hotmail.com>
2021-02-22 01:59:09 -0300

    Example file and updates to delete script

--- Unit 3 - Infrastructure as Code/Scripts/delete-stack.sh
+++ Unit 3 - Infrastructure as Code/Scripts/delete-stack.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-if [[ $# -ge 4 ]]
+if [[ $# -eq 2 ]]
 then
     stackStatus=$(aws cloudformation describe-stacks --stack-name ${1} --output text --query "Stacks[*].{Status:StackStatus}")
     if [[ $? -eq 0 ]]
     then
-        aws cloudformation delete-stack --stack-name ${1} --region=${4}
+        aws cloudformation delete-stack --stack-name ${1} --region=${2}
         if [[ $? -eq 0 ]]
         then
-            sleep 5
             stackStatus=$(aws cloudformation describe-stacks --stack-name ${1} --output text --query "Stacks[*].{Status:StackStatus}")
             while [[ $stackStatus == "DELETE_IN_PROGRESS" ]]
             do
                 sleep 5
                 stackStatus=$(aws cloudformation describe-stacks --stack-name ${1} --output text --query "Stacks[*].{Status:StackStatus}")
+                echo "Delete in progress, status: ${stackStatus}"
             done
+            echo "Delete completed"
         else
             echo "Delete failed: stack status is ${stackStatus}"
         fi
